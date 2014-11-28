7cf61c11b79aebd800c36a86df9cd3baf0af67b0
Roopesh Chander <roop@roopc.net>
2014-11-28 18:48:42 +0530

    Handle multi-byte code-points when using the source map

--- objc/shl.m
+++ objc/shl.m
@@ -25,22 +25,35 @@ static void shl_apply_formatting_with_srcmap(void *shl, srcmap_t* srcmap, size_t
         shlData.textFormatting = 0;
     }
     SyntaxHighlightArbiter *shlArbiter = (__bridge SyntaxHighlightArbiter *) shl;
-    size_t pos = *srcmap;
-    int i = 1, n = 0;
-    for (i = 1; i < length; i++) {
-        if (srcmap[i] != (pos + i - n)) {
-            if (srcmap[i - 1] >= 0) {
-                // This range can be mapped to the source
-                [shlArbiter ensureTextRange:NSMakeRange(pos, i - n) isSyntaxHighlightedWithData:shlData];
-            }
-            pos = srcmap[i];
-            n = i;
-        }
-    }
-    if (srcmap[length - 1] >= 0) {
-        // This range can be mapped to the source
-        [shlArbiter ensureTextRange:NSMakeRange(pos, i - n) isSyntaxHighlightedWithData:shlData];
-    }
+
+	srcmap_t begin = srcmap[0];
+	srcmap_t prev_sm = begin;
+	for (int i = 1; i < length; i++) {
+		srcmap_t sm = srcmap[i];
+		if (begin < 0) {
+			if (sm > 0) {
+				begin = sm; // Need a valid begin first
+			}
+		} else {
+			if (prev_sm >= 0) {
+				if ((sm < 0) || ((sm - prev_sm) > 1)) {
+					// A contiguous sequence ended at prev_sm
+					assert(begin >= 0);
+					if (prev_sm >= begin) {
+						NSRange stringRange = NSMakeRange(begin, prev_sm - begin + 1);
+						[shlArbiter ensureTextRange:stringRange isSyntaxHighlightedWithData:shlData];
+						begin = sm;
+					}
+				}
+			}
+		}
+		prev_sm = sm;
+	}
+
+	if (begin >= 0 && prev_sm >= begin) {
+		NSRange stringRange = NSMakeRange(begin, prev_sm - begin + 1);
+		[shlArbiter ensureTextRange:stringRange isSyntaxHighlightedWithData:shlData];
+	}
 }
 
 void shl_apply_syntax_formatting_with_srcmap(void *shl, srcmap_t* srcmap, size_t length, shl_syntax_formatting_t kind)
